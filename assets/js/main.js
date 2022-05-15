c5b62e27e04dc37a7fa02ae5204bae68b5cddfff
yahya <yahyahussein670@gmail.com>
2022-05-15 21:45:48 +0300

    update the value of the answer variable,retryquiz variables,the for loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,61 @@
-var startBtn= document.getElementById('startquiz');
-var quizBtn=document.getElementById('submit');
-var excellentScore = document.getElementById('excellentMarks');
-var averageScore = document.querySelector('#averageMarks');
-var poorScore =document.querySelector('#poorMarks');
-var retryQuiz = document.getElementById('retry');
+var startBtn = document.getElementById("startquiz");
+var quizBtn = document.getElementById("btnsubmit");
+var excellentScore = document.getElementById("excellentMarks");
+var averageScore = document.getElementById("averageMarks");
+var poorScore = document.getElementById("PoorMarks");
+var retryQuiz1 = document.getElementById("retry1");
+var retryQuiz2 = document.getElementById("retry2");
+var retryQuiz3 = document.getElementById("retry3");
 
-startBtn.addEventListener('click', sendEvent);
-quizBtn.addEventListener('click',calculationsEvent);
-retryQuiz.addEventListener('click',retryEvent);
+startBtn.addEventListener("click", sendEvent);
+quizBtn.addEventListener("click", calculationsEvent);
 
+var answers = ["JS", "var", "add", "Hypertextmarkuplanguage"];
 
-let totalCorrectScore = 0;
+retryQuiz1.addEventListener("click", retryEvent);
+retryQuiz2.addEventListener("click", retryEvent);
+retryQuiz3.addEventListener("click", retryEvent);
 
-const correctAnswers= ['JS', 'var', 'add', 'Hyper text markup language' ]
+let score = 0;
 
+var checked_answers = [];
 
 function sendEvent(e) {
-    e.preventDefault()
-    var firstPage = document.getElementById('pageOne')
+  e.preventDefault();
+  var firstPage = document.getElementById("pageOne");
 
-    var questionsPage = document.getElementById('quizPage')
+  var questionsPage = document.getElementById("quizgroup");
 
-    firstPage.style.display='none';
-    questionsPage.style.display='initial';
+  firstPage.style.display = "none";
+  questionsPage.style.display = "initial";
 }
+
+// calculationsEvent()
 
 function calculationsEvent(e) {
-    e.preventDefault()
+  e.preventDefault(e);
 
-    let questionOne = document.getElementById('questionOne')
-    let questionTwo = document.getElementById('questionTwo')
-    let questionThree = document.getElementById('questionThree')
-    let questionFour = document.getElementById('questionFour')
-    let totalQuestion = [questionOne, questionTwo, questionThree, questionFour]
+  let questionOne = document.getElementsByName("answerOne");
+  let questionTwo = document.getElementsByName("answerTwo");
+  let questionThree = document.getElementsByName("answerThree");
+  let questionFour = document.getElementsByName("answerFour");
+  let totalQuestion = [questionOne, questionTwo, questionThree, questionFour];
 
 
-    for (let h = 0; h < array.length; h++) {
-        for(var i=0; i<totalQuestion.length;i++){
-        for( var j=0; j<totalQuestion[i].length;j++){
-        if(quiz[i][j].checked){
-        if(quiz[i][j].value === answers[h]){
-        score++;
+  for(var h=0; h<answers.length;h++){
+
+    for (var i = 0; i < totalQuestion.length; i++) {
+      for (var j = 0; j < totalQuestion[i].length; j++) {
+        if (totalQuestion[i][j].checked) {
+          console.log("answer2",totalQuestion[i][j].value);
+          if (totalQuestion[i][j].value === answers[h]) {
+            checked_answers.push(totalQuestion[i][j].value);
+            score++;
+          }
         }
-        }
+      }
     }
+  }
 }
-    }
 
-}
+  
